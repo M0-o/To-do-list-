f512ab000d892b7e379c9117020412be6ed7acf3
Mouad Berrami <berrami.mouad2@gmail.com>
2023-12-17 21:57:02 +0100

    basic structure of the app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + "/views")) ;
+
+
+
+app.listen( 3000 , 
+     console.log("app listening on port 3000")
+
+);
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname + "/views/index.html");
+})
